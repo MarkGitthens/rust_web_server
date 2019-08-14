f2580fbef62a0ff9127a4267c32925d47ec68f80
Mark Gitthens <mgitthens@gmail.com>
2019-08-14 00:16:18 -0400

    Refactored data structures and updated associated logic.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::collections::HashMap;
 
+//TODO: Determine what fields should be nullable and update to be of type Option<T>
 #[derive(Debug)]
 struct RequestLine {
     method: String,
@@ -11,11 +12,18 @@ struct RequestLine {
 }
 
 #[derive(Debug)]
-struct HttpMessage {
+struct HttpHeaderInformation {
     status_line: RequestLine,
     header_fields: HashMap<String, String>,
 }
 
+#[derive(Debug)]
+struct HttpMessage {
+    header_info: HttpHeaderInformation,
+    payload: Option<String>,
+}
+
+//TODO: Need to benchmark practically everything here and will most likely need to make some optimizations
 fn main() {
     let listener: TcpListener = TcpListener::bind("localhost:7999").unwrap();
 
@@ -24,6 +32,8 @@ fn main() {
             Ok(stream) => {
                 let mut tcp_stream: TcpStream = stream;
 
+                //TODO: Need to determine a good buffer size for general http messages
+                //TODO: Need to plan for potentially needing multiple buffers worth of data for a single http messages
                 let mut buffer: [u8; 512] = [0; 512];
                 tcp_stream.read(&mut buffer).unwrap();
 
@@ -42,29 +52,32 @@ fn main() {
 
 //TODO: This will probably have to change for handling chunked data.
 fn parse_http_message(request: &mut [u8]) {
-    let request_body: String = format!("{}", String::from_utf8_lossy(&request[..]));
-    let message_data: Vec<&str> = request_body.split("\r\n\r\n").collect::<Vec<&str>>();
+    let http_message: String = format!("{}", String::from_utf8_lossy(&request[..]));
+    let split_message: Vec<&str> = http_message.split("\r\n\r\n").collect::<Vec<&str>>();
 
-    let result: (bool, HttpMessage) = parse_header_information(message_data[0]);
+    let result: (bool, HttpHeaderInformation) = parse_header_information(split_message[0]);
 
-    if !result.0 {
-        let message_object: HttpMessage = result.1;
+    let message_object: HttpMessage = if result.0 {
+        HttpMessage {
+            header_info: result.1,
+            payload: Some(String::from(split_message[1])),
+        }
+    } else {
+        HttpMessage {
+            header_info: result.1,
+            payload: None,
+        }
+    };
 
-        println!("{:?}\n\n{:?}", message_object.status_line, message_object.header_fields);
-        
-        //TODO: determine if we need to handle payload data or not
-        //let _data: &str = message_data[1];
+    match message_object.payload {
+        Some(payload) => {println!("{:?}\r\n\r\n{:?}", message_object.header_info, payload);},
+        None => {println!("{:?}", message_object.header_info);},
     }
 }
 
-//TODO: This should probably instead return whether the headers were valid or not.
-//Parses the header information and returns true if we should parse the data payload.
-fn parse_header_information(headers: &str) -> (bool, HttpMessage) {
-    //TODO: Research to see if we need to split on a single CR or single LF
-    //The http 1.1 std specifies that headers should be split using CRLF but I remember reading somewhere
-    //that historically people have used just CR or just LF. Need to verify this
+fn parse_header_information(headers: &str) -> (bool, HttpHeaderInformation) {
 
-    //Reverse vector so that we can use it as a stack for simpler logic
+    //Reverse vector so that we can use it as a stack to simplify processing
     let mut header_lines: Vec<&str> = headers.split("\r\n").collect();    
     header_lines.reverse();
 
@@ -72,11 +85,11 @@ fn parse_header_information(headers: &str) -> (bool, HttpMessage) {
 
     //TODO: Add sanity checking and return proper error code if invalid data
     //TODO: Add logic to determine if this is a request or a response message
-    let mut result: HttpMessage = HttpMessage {
+    let mut result: HttpHeaderInformation = HttpHeaderInformation {
         status_line: RequestLine { 
-                        method: String::from(start[0]),
-                        target: String::from(start[1]),
-                        version: String::from(start[2]), },
+            method: String::from(start[0]),
+            target: String::from(start[1]),
+            version: String::from(start[2]), },
         header_fields: HashMap::new(),
     };
 
@@ -84,16 +97,17 @@ fn parse_header_information(headers: &str) -> (bool, HttpMessage) {
         let field: Option<&str> = header_lines.pop();
         match field {
             Some(res) => {
-                //TODO: Can't split on : because some fields use this character in the field value
-                //Fix probably involves recombining the result of split from [1..end] for a generic field
-                //Need to determine if all fields can use : as a valid character or if only a select few can
+                //TODO: Should I provide better parsing for field values or leave that up to the user?
                 let split_field: Vec<&str> = res.split(":").collect();
-                result.header_fields.insert(String::from(split_field[0]), String::from(split_field[1]));
+                let field_value: String = split_field[1..].join(":");
+
+                result.header_fields.insert(String::from(split_field[0]), field_value.trim().to_string());
             },
             None => { continue; },
         }
     }
 
+    //Default to return false since we aren't yet checking if we need to process a payload or not
     return (false, result);
 }
 
