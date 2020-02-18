bd42100d27fe6f2a33d0f6ae1e6fa341bab74031
Mark Gitthens <mgitthens@gmail.com>
2020-02-17 21:04:24 -0600

    Testing handling jpg files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+extern crate dirs;
+
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::collections::HashMap;
-use std::path::Path;
+use std::path::PathBuf;
 use std::fs::File;
 use std::error::Error;
+use std::str;
 
 #[derive(Debug)]
 enum RequestMethod {
@@ -66,8 +69,8 @@ fn main() {
                 let mut buffer: [u8; 512] = [0; 512];
                 tcp_stream.read(&mut buffer).unwrap();
 
-                parse_http_message(&mut buffer);
-                let headers = parse_request(&mut buffer);               
+                let message: HttpMessage = parse_http_message(&mut buffer);
+                let headers = parse_request(message);               
 
                 tcp_stream.write(headers.as_bytes()).unwrap();
                 tcp_stream.flush().unwrap();                
@@ -80,7 +83,7 @@ fn main() {
 }
 
 //TODO: This will probably have to change for handling chunked data.
-fn parse_http_message(request: &mut [u8]) {
+fn parse_http_message(request: &mut [u8]) -> HttpMessage {
     let http_message: String = format!("{}", String::from_utf8_lossy(&request[..]));
     let split_message: Vec<&str> = http_message.split("\r\n\r\n").collect::<Vec<&str>>();
 
@@ -102,18 +105,12 @@ fn parse_http_message(request: &mut [u8]) {
         }
     };
 
-    match message_object.payload {
-        Some(payload) => {println!("{:?}\r\n\r\n{:?}", message_object.header_info, payload);},
-        None => {},
-    }
+    return message_object;
 }
 
 fn parse_header_information(headers: &str) -> (bool, HttpHeaderInformation) {
-    //Reverse vector so that we can use it as a stack to simplify processing
-    let mut header_lines: Vec<&str> = headers.split("\r\n").collect();    
-    header_lines.reverse();
-
-    let start: Vec<&str> = header_lines.pop().unwrap().split(" ").collect();
+    let header_lines: Vec<&str> = headers.split("\r\n").collect();
+    let start: Vec<&str> = header_lines[0].split(" ").collect();
 
     //TODO: Probably don't need to initialize both of these
     let mut _request_line: RequestLine = RequestLine {
@@ -127,8 +124,7 @@ fn parse_header_information(headers: &str) -> (bool, HttpHeaderInformation) {
         status_code: 0,
         reason_phrase: String::from(""),
     };
- 
-    //TODO: Maybe setup a basic callback system that gets triggered here.
+
     match start[0] {
         "GET" => {_request_line.method = RequestMethod::GET;}
         "HEAD" => { _request_line.method = RequestMethod::HEAD;}
@@ -160,18 +156,14 @@ fn parse_header_information(headers: &str) -> (bool, HttpHeaderInformation) {
         }
     }
 
-    while !header_lines.is_empty(){
-        let field: Option<&str> = header_lines.pop();
-        match field {
-            Some(res) => {
-                //TODO: Should I provide better parsing for field values or leave that up to the user?
-                let split_field: Vec<&str> = res.split(":").collect();
-                let field_value: String = split_field[1..].join(":");
+    for x in 1..header_lines.len() {
+        let field: &str = header_lines[x];
 
-                result.header_fields.insert(String::from(split_field[0]), field_value.trim().to_string());
-            },
-            None => { continue; },
-        }
+        //TODO: Should I provide better parsing for field values or leave that up to the user?
+        let split_field: Vec<&str> = field.split(":").collect();
+        let field_value: String = split_field[1..].join(":");
+
+        result.header_fields.insert(String::from(split_field[0]), field_value.trim().to_string());
     }
 
     match result.header_fields.get("Content-Length") {
@@ -181,13 +173,10 @@ fn parse_header_information(headers: &str) -> (bool, HttpHeaderInformation) {
 }
 
 //Temporary helper function for testing
-fn parse_request(request: &mut [u8]) -> String {
-    let get = b"GET / HTTP/1.1\r\n";
-    let mut response: String = String::from("");
-    
-    if request.starts_with(get) {
-        response = format!("{}{}", build_headers(), build_response());
-    } 
+fn parse_request(request: HttpMessage) -> String {    
+    let req_line = request.header_info.request_line.unwrap();
+
+    let response = format!("{}{}", build_headers(), build_response(req_line.target));
 
     return response;
 }
@@ -202,9 +191,24 @@ fn set_file(f1: File, file: &mut Option<File>) {
 
 //Temporary helper function for testing
 //TODO: This seems really REALLY gross probably misusing match super hard here
-fn build_response() -> String {
+//TODO: Probably vulnerable to Path Traversal exploits
+fn build_response(uri: String) -> String {
     //TODO: have this be configurable through a config file
-    let path  = Path::new("/home/mark/html/static/example.html");
+    let mut path: PathBuf = match dirs::home_dir() {
+        Some(x) => x,
+        None => PathBuf::new()
+    };
+
+    let split_message: Vec<&str> = uri.split(".").collect::<Vec<&str>>();
+
+    if split_message.len() <= 1 {
+        //STUB
+    }
+
+    path.push("html/static");
+
+    //skip the first forward slash
+    path.push(&uri[1..]);
 
     let mut response: String = String::new();
     let file = File::open(&path);
@@ -215,21 +219,43 @@ fn build_response() -> String {
         Ok(resp) => set_file(resp, &mut f),
     };
 
-    if response.is_empty() {
-        match f {
-            Some(mut x) => match x.read_to_string(&mut response) {
-                _ => ()
-            },
-            None => ()
-        };
+    let content_type: String;
+    let mut con_len: usize = 0;
+
+    if split_message[1] == "html" {
+        content_type = String::from("Content-Type: text/html\r\n");
+        if response.is_empty() {
+            match f {
+                Some(mut x) => match x.read_to_string(&mut response) {
+                    Ok(x) => con_len = x,
+                    _ => con_len = 0
+                },
+                None => ()
+            };
+        }
+    }
+    else if split_message[1] == "jpg"{        
+        content_type = String::from("Content-Type: image/jpeg\r\n");
+        let mut buf = Vec::new();
+
+        if response.is_empty() {
+            match f.unwrap().read_to_end(&mut buf) {
+                Ok(x) => con_len = x,
+                Err(e) => {con_len = 0; println!("Read failed {}", e.description())}
+            };
+        }
+
+        response = unsafe {str::from_utf8_unchecked(&buf).to_string()};
+    } else {
+        content_type = String::from("Content-Type: text/html\r\n");
+        file_does_not_exist(&mut response);
+        con_len = response.len();
     }
 
-    let content_type: String = String::from("Content-Type: text/html\r\n");
-    let content_length: String = format!("Content-Length: {}\r\n\r\n", response.len());
+    let content_length: String = format!("Content-Length: {}\r\n\r\n", con_len);
 
     return format!("{}{}{}", content_type, content_length, response);
 }
-
 
 //Temporary helper function for testing
 fn build_headers() -> String {
